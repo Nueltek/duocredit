453c94d24c487db05a6d9a8f57e07d8c2d655875
nueltek <emmathics@gmail.com>
2025-12-29 23:14:06 -1200

    fixed horizontal srolling on mobile view

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={poppins.variable}>
-      <body className="antialiased">{children}</body>
+      <body className="antialiased overflow-x-hidden">{children}</body>
     </html>
   );
 }
